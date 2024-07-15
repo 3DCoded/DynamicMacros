2f60fce45e65a3584d8f7ad3d317b853c8fbac39
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-07-15 14:18:24 -0400

    fix typo

--- dynamicmacros.py
+++ dynamicmacros.py
@@ -77,7 +77,7 @@ class DynamicMacro:
     def from_section(config, section, printer):
         raw = config.get(section, 'gcode')
         name = section.split()[1]
-        desc = config.get(section, 'description', '')
+        desc = config.get(section, 'description') or ''
         return DynamicMacro(name, raw, printer, desc=desc)
     
     def run(self, params, rawparams):
