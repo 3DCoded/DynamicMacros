54b16ad56a78a6a21fdd7aed01380a3471677e46
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-07-15 14:17:26 -0400

    attempt to add renamable/deletable/addable macros, with descriptions

--- dynamicmacros.py
+++ dynamicmacros.py
@@ -15,12 +15,22 @@ class DynamicMacros:
         
         self.gcode.register_command(
             'DYNAMIC_MACRO',
-            self.cmd_DYNAMIC_MACRO
+            self.cmd_DYNAMIC_MACRO,
+            desc='Run Dynamic Macro'
         )
 
         self._update_macros()
-        for name in self.macros.keys():
-            self.gcode.register_command(name.upper(), self.generate_cmd(name))
+        # for name in self.macros.keys():
+        #     self.gcode.register_command(name.upper(), self.generate_cmd(name))
+    
+    def register_macro(self, macro):
+        self.gcode.register_command(macro.name.upper(), self.generate_cmd(macro.name.upper()), desc=macro.desc)
+        self.macros[macro.name] = macro
+
+    def unregister_macro(self, macro):
+        if macro in self.macros:
+            self.gcode.register_command(macro.name.upper())
+            del self.macros[macro.name]
     
     def cmd_DYNAMIC_MACRO(self, gcmd):
         macro = gcmd.get('MACRO')
@@ -41,6 +51,8 @@ class DynamicMacros:
         macro.run(params, rawparams)
     
     def _update_macros(self):
+        for macro in self.macros:
+            self.unregister_macro(macro)
         for fname in self.fnames:
             path = config_path / fname
             config = configparser.RawConfigParser()
@@ -48,13 +60,16 @@ class DynamicMacros:
             for section in config.sections():
                 if section.split()[0] == 'gcode_macro':
                     name = section.split()[1]
-                    self.macros[name] = DynamicMacro.from_section(config, section, self.printer)
+                    macro = DynamicMacro.from_section(config, section, self.printer)
+                    self.macros[name] = macro
+                    self.register_macro(macro)
 
 class DynamicMacro:
-    def __init__(self, name, raw, printer):
+    def __init__(self, name, raw, printer, desc=''):
         self.name = name
         self.raw = raw
         self.printer = printer
+        self.desc = desc
         self.env = jinja2.Environment('{%', '%}', '{', '}')
         self.template = TemplateWrapper(self.printer, self.env, self.name, self.raw)
         self.variables = {}
@@ -62,7 +77,8 @@ class DynamicMacro:
     def from_section(config, section, printer):
         raw = config.get(section, 'gcode')
         name = section.split()[1]
-        return DynamicMacro(name, raw, printer)
+        desc = config.get(section, 'description', '')
+        return DynamicMacro(name, raw, printer, desc=desc)
     
     def run(self, params, rawparams):
         kwparams = dict(self.variables)
