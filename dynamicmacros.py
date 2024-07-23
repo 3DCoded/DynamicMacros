c1b084e356c56e0dbee842dc5fd6686e64a9bbb0
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-07-23 09:06:06 -0400

    EXPERIMENTAL: allow standard gcode_macro variables to be used, retreived, updated, and parsed in Dynamic Macros

--- dynamicmacros.py
+++ dynamicmacros.py
@@ -3,6 +3,8 @@ from .gcode_macro import TemplateWrapper
 from pathlib import Path
 import configparser
 import os
+import ast
+import json
 
 config_path = Path(os.path.expanduser('~')) / 'printer_data' / 'config'
 
@@ -13,7 +15,7 @@ class DynamicMacros:
         self.gcode = self.printer.lookup_object('gcode')
         self.fnames = config.getlist('configs')
         self.macros = {}
-        self.placeholder = DynamicMacro('Placeholder', 'M117 ERROR', self.printer)
+        self.placeholder = DynamicMacro('Error', 'RESPOND MSG="ERROR"', self.printer)
         
         self.gcode.register_command(
             'DYNAMIC_MACRO',
@@ -27,6 +29,8 @@ class DynamicMacros:
         self.macros[macro.name] = macro
         if (macro.name not in self.gcode.ready_gcode_handlers) and (macro.name not in self.gcode.base_gcode_handlers):
             self.gcode.register_command(macro.name.upper(), self.generate_cmd(macro), desc=macro.desc)
+            self.gcode.register_mux_command('SET_GCODE_VARIABLE', 'MACRO', macro.name, macro.cmd_SET_GCODE_VARIABLE, desc="Set the value of a G-Code macro variable")
+            self.printer.objects[f'gcode_macro {macro.name}'] = macro
 
     def unregister_macro(self, macro):
         self.gcode.register_command(macro.name.upper(), None)
@@ -48,7 +52,6 @@ class DynamicMacros:
             gcmd.respond_info(f'Message: {msg}')
         except Exception as e:
             gcmd.respond_error(str(e))
-    
     
     def generate_cmd(self, macro):
         def cmd(gcmd):
@@ -75,12 +78,13 @@ class DynamicMacros:
                     self.register_macro(macro)
 
 class DynamicMacro:
-    def __init__(self, name, raw, printer, desc=''):
+    def __init__(self, name, raw, printer, desc='', variables={}):
         self.name = name
         self.raw = raw
         self.printer = printer
         self.desc = desc
-        self.variables = {}
+        self.variables = variables
+        self.vars = {}
 
         self.gcodes = self.raw.split('\n\n\n')
         self.templates = []
@@ -92,7 +96,7 @@ class DynamicMacro:
         return TemplateWrapper(self.printer, env, self.name, gcode)
     
     def update(self, name, val):
-        self.variables[name] = val
+        self.vars[name] = val
         return val
 
     def get_macro_variables(self, macro_name):
@@ -100,14 +104,32 @@ class DynamicMacro:
         return macro.variables
 
     def update_from_dict(self, dictionary):
-        self.variables.update(dictionary)
+        self.vars.update(dictionary)
         return dictionary
     
-    def from_section(config, section, printer):
+    def cmd_SET_GCODE_VARIABLE(self, gcmd):
+        variable = gcmd.get('VARIABLE')
+        value = gcmd.get('VALUE')
+        if variable not in self.variables:
+            raise gcmd.error("Unknown gcode_macro variable '%s'" % (variable,))
+        try:
+            literal = ast.literal_eval(value)
+            json.dumps(literal, separators=(',', ':'))
+        except (SyntaxError, TypeError, ValueError) as e:
+            raise gcmd.error("Unable to parse '%s' as a literal: %s" %
+                             (value, e))
+        self.variables[variable] = literal
+    
+    def from_section(config: configparser.RawConfigParser, section, printer):
         raw = config.get(section, 'gcode')
         name = section.split()[1]
         desc = config.get(section, 'description') if config.has_option(section, 'description') else 'No Description'
-        return DynamicMacro(name, raw, printer, desc=desc)
+        variables = {}
+        for key, value in config.items(section):
+            if key.startswith('variable_'):
+                variable = key[len('variable_'):]
+                variables[variable] = value
+        return DynamicMacro(name, raw, printer, desc=desc, variables=variables)
     
     def run(self, params, rawparams):
         for template in self.templates:
@@ -115,6 +137,7 @@ class DynamicMacro:
     
     def _run(self, template, params, rawparams):
         kwparams = dict(self.variables)
+        kwparams.update(self.vars)
         kwparams.update(template.create_template_context())
         kwparams['params'] = params
         kwparams['rawparams'] = rawparams
