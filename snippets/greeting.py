f63de93ef940c71f49a65c4dc47d066dc7318881
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-08-07 16:18:22 -0400

    Deployed ffa6a1c with MkDocs version: 1.6.0

--- snippets/greeting.py
+++ snippets/greeting.py
@@ -1,7 +1,7 @@
 # --8<-- [start:code]
 # --8<-- [start:cls]
 # --8<-- [start:clsheader]
-class Greeter:
+class Greeting:
 # --8<-- [end:clsheader]
     # --8<-- [start:init]
     # --8<-- [start:initheader]
@@ -11,21 +11,30 @@ class Greeter:
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
         self.gcode = self.printer.lookup_object('gcode')
+        self.name = config.get_name().split()[1]
 
         self.message = config.get('message', 'Welcome to Klipper!')
+        self.delay = config.get_int('delay', 0)
         # --8<-- [end:readcfg]
 
         # --8<-- [start:regcmd]
-        self.gcode.register_command(
-            'GREET', self.cmd_GREET, desc=self.cmd_GREET_help)
-        self.printer.register_event_handler(
+        self.gcode.register_mux_command(
+            'GREETING',
+            'NAME',
+            self.name,
+            self.cmd_GREETING,
+            desc=self.cmd_GREETING_help
+        )
+        
+        if self.delay > 0:
+            self.printer.register_event_handler(
             'klippy:ready', self._ready_handler)
         # --8<-- [end:regcmd]
     # --8<-- [end:init]
 
     # --8<-- [start:handler]
     def _ready_handler(self):
-        waketime = self.reactor.monotonic() + 1
+        waketime = self.reactor.monotonic() + self.delay
         self.reactor.register_timer(self._greet, waketime)
     # --8<-- [end:handler]
 
@@ -36,14 +45,14 @@ class Greeter:
     # --8<-- [end:agreet]
 
     # --8<-- [start:greet]
-    cmd_GREET_help = "Greet the user"
-    def cmd_GREET(self, gcmd):
+    cmd_GREETING_help = "Greet the user"
+    def cmd_GREETING(self, gcmd):
         self._greet()
     # --8<-- [end:greet]
 # --8<-- [end:cls]
 
 # --8<-- [start:loadcfg]
-def load_config(config):
-    return Greeter(config)
+def load_config_prefix(config):
+    return Greeting(config)
 # --8<-- [end:loadcfg]
 # --8<-- [end:code]
