22fa2627343b0fe9736719a005e406968f3b807b
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-10-10 10:00:26 -0400

    fix: Safely unregister macro if macro already exists to prepare for re-registration of new macro

--- dynamicmacros.py
+++ dynamicmacros.py
@@ -80,6 +80,7 @@ class DynamicMacros:
     def register_macro(self, macro):
         self.macros[macro.name.upper()] = macro
         if (macro.name not in self.gcode.ready_gcode_handlers) and (macro.name not in self.gcode.base_gcode_handlers):
+            _ = self.gcode.register_command(macro.name.upper(), None, desc=macro.desc)
             self.gcode.register_command(
                 macro.name.upper(), self.generate_cmd(macro), desc=macro.desc)
             if isinstance(macro, DelayedDynamicMacro):
